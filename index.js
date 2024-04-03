de4a3782ceb658ea794b8dd16cc6d4b2fbae90e7
levis <levismunene01@gmail.com>
2024-04-03 15:14:36 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(booksData => renderBooks(booksData))
+    .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
